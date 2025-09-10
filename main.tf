6c122f51fba8abcdb28057ac8a01aadc377fb6f0
Aung San Oo <25-00117@dat.com>
2025-09-10 10:11:57 +0630

    update backend  main.tf file

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+terraform {
+  cloud {
+    organization = "aungsanoo"
+    workspaces {
+      name = "dailyreportapp"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+  region = "ap-southeast-1"
+  
+}
 resource "aws_instance" "webserver" {
     ami = "ami-0b8607d2721c94a77"
     instance_type = "t2.micro"
